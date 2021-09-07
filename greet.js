316902148c04ada065e17f1bb48a8c4c1e6b5881
Ramahadi Jan <moheanerj@gmail.com>
2021-09-07 08:35:42 +0200

    added andused faglet module

--- greet.js
+++ greet.js
@@ -1,6 +1,14 @@
 
 const c = require('chalk')
+const f = require('figlet')
 
 module.exports =function greet(name){
+  console.log(f.textSync(name, {
+    font: 'Delta Corps Priest 1',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
     return(c.bgGreenBright("Hello, "+ name))
   }
